e81761a8e1b6d357a12327f598c82e76638772e0
Khaleel Sheikh <140696327+khaleelsheikh@users.noreply.github.com>
2024-08-09 01:36:57 +0300

    second commit

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import StyleGuide from "@/components/StyleGuide";
 export default function Home() {
   return (
     <main className="w-full max-w-[1440px] bg-white mx-auto overflow-hidden">
-      <StyleGuide/>
+      <StyleGuide />
     </main>
   );
 }
